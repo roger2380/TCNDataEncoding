8317a92a733fe1036b5a2555a4251a9cdccce945
唐艺源 <tyy@shtruecolor.com>
2017-07-06 11:15:52 +0800

    Initial Commit

---
+++ TCNDataCommon/TCNDataCommon.h
@@ -0,0 +1,13 @@
+//
+//  TCNDataCommon.h
+//  TCNDataCommon
+//
+//  Created by tangyiyuan on 2017/7/6.
+//  Copyright © 2017年 tangyiyuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCNDataCommon : NSObject
+
+@end
---
+++ TCNDataCommon/TCNDataCommon.m
@@ -0,0 +1,13 @@
+//
+//  TCNDataCommon.m
+//  TCNDataCommon
+//
+//  Created by tangyiyuan on 2017/7/6.
+//  Copyright © 2017年 tangyiyuan. All rights reserved.
+//
+
+#import "TCNDataCommon.h"
+
+@implementation TCNDataCommon
+
+@end
