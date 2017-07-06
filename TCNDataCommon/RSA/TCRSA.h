e9c467e17da840e03d850defe2fb89d304dd126a
唐艺源 <tyy@shtruecolor.com>
2017-07-06 14:24:57 +0800

    first commit

---
+++ TCNDataCommon/RSA/TCRSA.h
@@ -0,0 +1,14 @@
+//
+//  TCRSA.h
+//  TCCommon
+//
+//  Created by YongQing Gu on 8/13/12.
+//  Copyright (c) 2012 TrueColor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCRSA : NSObject
++ (NSData*) encrypt:(NSString*) string;
++ (NSString*) doOffset:(NSString*) string offset:(NSInteger) offset;
+@end
